7fec86765c0564ba1e9da476f3f66cfdc71fe51e
rammohan <rammohan8555@gmail.com>
2024-12-23 15:32:03 +0530

    python

---
+++ Day4/calculator.py
@@ -0,0 +1,17 @@
+
+
+def addition(num1,num2):
+    add = num1 + num2
+    return add
+
+def sub(num1,num2):
+    s = num1 - num2
+    return s
+
+def multi(num1,num2):
+    m = num1 * num2
+    return m
+
+print(addition(3, 6))
+print(sub(4,8))
+print(multi(5,6))
